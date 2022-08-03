a550daf2489510e663e71e80bfe95c94cb924a18
DavideLopez <davidelopez7@gmail.com>
2022-08-03 15:55:03 +0200

    creata struttura e inserito array

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 console.log('js-our-team')
+
+// creiamo un array per gli oggetti
+const team = [
+    {
+        name : 'Angela Caroll'
+        role : 'Chief Editor'
+        image : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name : 'Angela Lopez'
+        role : 'Social Media Manager'
+        image : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name : 'Barbara Ramos'
+        role : 'Graphic Designer'
+        image : 'barbara-ramos-graphic-designer.jpg'
+    },
+    {
+        name : 'Scott Estrada'
+        role : 'Developer'
+        image : 'scott-estrada-developer.jpg'
+    },
+    {
+        name : 'Walter Gordon'
+        role : 'Office Manager'
+        image : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name : 'Wayne barret'
+        role : 'Founder & CEO'
+        image : 'wayne-barnet-founder-ceo.jpg'
+    },
+    console.log(team)
+];
