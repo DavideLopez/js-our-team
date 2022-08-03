2990421f066023d29511bdd28e22a02eab256628
DavideLopez <davidelopez7@gmail.com>
2022-08-03 16:07:20 +0200

    aggiunto stile e ciclo for

--- app.js
+++ app.js
@@ -3,34 +3,48 @@ console.log('js-our-team')
 // creiamo un array per gli oggetti
 const team = [
     {
-        name : 'Angela Caroll'
-        role : 'Chief Editor'
-        image : 'angela-caroll-chief-editor.jpg'
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
     },
     {
-        name : 'Angela Lopez'
-        role : 'Social Media Manager'
-        image : 'angela-lopez-social-media-manager.jpg'
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
     },
     {
-        name : 'Barbara Ramos'
-        role : 'Graphic Designer'
-        image : 'barbara-ramos-graphic-designer.jpg'
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
     },
     {
-        name : 'Scott Estrada'
-        role : 'Developer'
-        image : 'scott-estrada-developer.jpg'
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
     },
     {
-        name : 'Walter Gordon'
-        role : 'Office Manager'
-        image : 'walter-gordon-office-manager.jpg'
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
     },
     {
-        name : 'Wayne barret'
-        role : 'Founder & CEO'
-        image : 'wayne-barnet-founder-ceo.jpg'
-    },
-    console.log(team)
+        name: 'Wayne barret',
+        role: 'Founder & CEO',
+        image: 'wayne-barnet-founder-ceo.jpg'
+    }
 ];
+
+// console.log(team)
+
+//   collega l'array alla classe team
+const teamDom = document.querySelector('.team')
+
+//stampiamo gli oggetti nella console
+
+for ( let i = 0; i < team.length; i++) {
+    console.log(team[i][name],team[i][role], team[i][image])
+
+    //creiamo anche una funzione
+    showTeamMembers(team[i])
+
+}
